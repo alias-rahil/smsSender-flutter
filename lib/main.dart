4945b6baaa22c6334cd6cd7d639fc10a40f81209
alias-rahil <rahil.kabani.4@gmail.com>
2020-06-29 12:52:03 +0530

    feat: basic sms functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sms/sms.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SmsSender sender = new SmsSender();
+  String address = '+917984981764';
+  sender.sendSms(new SmsMessage(address, 'Hello flutter!'));
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
