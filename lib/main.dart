c14033081f471786ab06ed3f3307a558e223739e
alias-rahil <59060219+alias-rahil@users.noreply.github.com>
2020-07-02 14:56:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:sms/sms.dart';
 
 void main() {
-  SmsSender sender = new SmsSender();
-  String address = '+917984981764';
-  sender.sendSms(new SmsMessage(address, 'Hello flutter!'));
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    SmsSender sender = new SmsSender();
+    String address = '+917984981764';
+    sender.sendSms(new SmsMessage(address, 'Hello flutter!'));
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
