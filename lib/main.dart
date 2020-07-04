a81fbce817aa6f1d3ee29be287c46e9d6c3ab2ce
Your Name <you@example.com>
2020-07-04 11:27:06 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    SmsSender sender = new SmsSender();
     String address = '+917984981764';
+    SmsSender sender = new SmsSender();
     sender.sendSms(new SmsMessage(address, 'Hello flutter!'));
     return MaterialApp(
       title: 'Welcome to Flutter',
